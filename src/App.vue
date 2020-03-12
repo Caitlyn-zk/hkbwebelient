c147bc4df1304d1a133837331c2efe72a7e657cc
PC-2019HWL\Administrator <2655816316@qq.com>
2020-03-12 17:53:15 +0800

    项目搭建完成 邹凯 2020.3.12

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+    <el-button type="">常规样式</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="less">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/store/index.js
@@ -0,0 +1,10 @@
+// index.js
+import Vue from 'vue'
+import Vuex from 'vuex'
+import state from './state.js'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state
+})
